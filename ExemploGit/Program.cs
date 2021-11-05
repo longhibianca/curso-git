e53d9c8991c1c5c0af3fba5480d02884f5a7848f
Bianca Longhi <longhisbianca@gmail.com>
2021-11-04 22:55:04 -0300

    Segundo commit

--- ExemploGit/Program.cs
+++ ExemploGit/Program.cs
@@ -7,6 +7,8 @@ namespace ExemploGit
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            Console.WriteLine("Adicionada linha para segundo commit de teste");
         }
     }
 }
